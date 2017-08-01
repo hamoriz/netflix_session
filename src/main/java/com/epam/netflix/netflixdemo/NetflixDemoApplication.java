326e44abc0ee7eab1016b5cb95e803ef79b1bc1b
Zoltan_Hamori <zoltan.hamori78@gmail.com>
2017-08-01 21:06:09 +0200

    Initial commit

---
+++ src/main/java/com/epam/netflix/netflixdemo/NetflixDemoApplication.java
@@ -0,0 +1,12 @@
+package com.epam.netflix.netflixdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixDemoApplication.class, args);
+	}
+}
